42140a556ba5bbf0b40811b64bfde78ec60e778e
vinod1188 <vinod118888@gmail.com>
2024-01-15 17:12:04 +0530

    test commit

---
+++ src/main/java/test1/Main.java
@@ -0,0 +1,11 @@
+package test1.testid;
+
+/**
+ *
+ * @author Your name
+ */
+public class Main {
+    public static void main(String... param) {
+        System.out.println("Starting application...");
+    }
+}
